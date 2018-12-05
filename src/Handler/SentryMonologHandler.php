ffe408c088dae96455a33873daed8b6280d61d66
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2018-12-06 11:29:58 +1300

    BUG Show actual stacktrace for errors / exceptions

--- src/Handler/SentryMonologHandler.php
+++ src/Handler/SentryMonologHandler.php
@@ -9,40 +9,40 @@
 
 namespace PhpTek\Sentry\Handler;
 
-use PhpTek\Sentry\Monolog\Handler\SentryRavenHandler,
-    Monolog\Logger,
-    SilverStripe\Dev\Backtrace,
-    SilverStripe\Security\Member,
-    PhpTek\Sentry\Log\SentryLogger;
+use Exception;
+use Monolog\Logger;
+use PhpTek\Sentry\Log\SentryLogger;
+use PhpTek\Sentry\Monolog\Handler\SentryRavenHandler;
+use SilverStripe\Dev\Backtrace;
+use SilverStripe\Security\Member;
 
 /**
  * Monolog Handler for Sentry via Raven
  */
-
 class SentryMonologHandler extends SentryRavenHandler
-{    
+{
     /**
      * @var SentryClientAdaptor
      */
     protected $client;
 
     /**
-     * @param  int   $level
-     * @param  bool  $bubble
+     * @param  int $level
+     * @param  bool $bubble
      * @param  array $extras Extra parameters that will become "tags" in Sentry.
      * @return void
      */
     public function __construct($level = Logger::DEBUG, $bubble = true, $extras = [])
-    {        
+    {
         // Returns an instance of {@link SentryLogger}
         $logger = SentryLogger::factory($extras);
         $sdk = $logger->client->getSDK();
         $this->client = $logger->client;
         $this->client->setData('user', $this->getUserData(null, $logger));
-        
+
         parent::__construct($sdk, $level, $bubble);
     }
-    
+
     /**
      * @return SentryClientAdaptor
      */
@@ -50,14 +50,14 @@ class SentryMonologHandler extends SentryRavenHandler
     {
         return $this->client;
     }
-    
+
     /**
-     * write() forms the entry point into the physical sending of the error. The 
+     * write() forms the entry point into the physical sending of the error. The
      * sending itself is done by the current adaptor's `send()` method.
-     * 
-     * @param  array $record An array of error-context metadata with the following 
+     *
+     * @param  array $record An array of error-context metadata with the following
      *                       available keys:
-     * 
+     *
      *                       - message
      *                       - context
      *                       - level
@@ -66,53 +66,54 @@ class SentryMonologHandler extends SentryRavenHandler
      *                       - datetime
      *                       - extra
      *                       - formatted
-     * 
+     *
      * @return void
      */
     protected function write(array $record)
-    {   
-        // The complete compliment of these data come via the Raven_Client::xxx_context() methods
-        // Monolog does not make use of the 'extra' parameter, so we munge ours into
-        // its 'context' param and use that
-        if (!$extra = !empty($record['extra']) ? $record['extra'] : []) {
-            $extra = !empty($record['context']) ? $record['context'] : [];
-        }
-
-        $record = [
-            'level'      => $record['level'],
-            'formatted'  => $record['formatted'],
-            'channel'    => $record['channel'],
+    {
+        $record = array_merge($record, [
             'timestamp'  => $record['datetime']->getTimestamp(),
-            'extra'      => $extra,
             'stacktrace' => $this->backtrace($record),
-            'stack'      => true,
-        ];
-        
+        ]);
+
         // write() calls one of RavenHandler::captureException() or RavenHandler::captureMessage()
         // depending on if $record['context']['exception'] is an instance of Exception or not
         parent::write($record);
     }
-    
+
     /**
      * Generate a cleaned-up backtrace of the event that got us here.
-     * 
+     *
      * @param  array $record
      * @return array
      */
     private function backtrace($record)
     {
+        // Provided trace
+        if (!empty($record['context']['trace'])) {
+            return $record['context']['trace'];
+        }
+
+        // Generate trace from exception
+        if (isset($record['context']['exception'])) {
+            /** @var Exception $exception */
+            $exception = $record['context']['exception'];
+            return $exception->getTrace();
+        }
+
+        // Failover: build custom trace
         $bt = debug_backtrace();
-            
+
         // Push current line into context
         array_unshift($bt, [
-            'file' => !empty($bt['file']) ? $bt['file'] : 'N/A',
-            'line' => !empty($bt['line']) ? $bt['line'] : 'N/A',
+            'file'     => !empty($bt['file']) ? $bt['file'] : 'N/A',
+            'line'     => !empty($bt['line']) ? $bt['line'] : 'N/A',
             'function' => '',
-            'class' => '',
-            'type' => '',
-            'args' => [],
+            'class'    => '',
+            'type'     => '',
+            'args'     => [],
         ]);
-        
+
         $bt = Backtrace::filter_backtrace($bt, [
             '',
             'Monolog\\Handler\\AbstractProcessingHandler->handle',
@@ -122,14 +123,14 @@ class SentryMonologHandler extends SentryRavenHandler
             'PhpTek\\Sentry\\Handler\\SentryMonologHandler->write',
             'PhpTek\\Sentry\\Handler\\SentryMonologHandler->backtrace',
         ]);
-        
+
         return $bt;
     }
-    
+
     /**
      * Returns a default set of additional data specific to the user's part in
      * the request.
-     * 
+     *
      * @param  Member $member
      * @return array
      */
@@ -138,12 +139,11 @@ class SentryMonologHandler extends SentryRavenHandler
         if (!$member) {
             $member = Member::currentUser();
         }
-        
+
         return [
-            'IPddress'  => $logger->getIP(),
-            'ID'        => $member ? $member->getField('ID') : SentryLogger::SLW_NOOP,
-            'Email'     => $member ? $member->getField('Email') : SentryLogger::SLW_NOOP,
+            'IPddress' => $logger->getIP(),
+            'ID'       => $member ? $member->getField('ID') : SentryLogger::SLW_NOOP,
+            'Email'    => $member ? $member->getField('Email') : SentryLogger::SLW_NOOP,
         ];
     }
-    
 }
