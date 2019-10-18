fe9ac51c7ae169948a3457759b6bd2eb45656a6e
Russell Michell <russ@theruss.com>
2019-10-18 17:34:46 +1300

    FIX: Fixes #36 with greater range of severity detection.

--- src/Handler/SentryHandler.php
+++ src/Handler/SentryHandler.php
@@ -22,6 +22,7 @@ use PhpTek\Sentry\Adaptor\SentryAdaptor;
  */
 class SentryHandler extends AbstractProcessingHandler
 {
+
     use Injectable;
 
     /**
@@ -60,17 +61,17 @@ class SentryHandler extends AbstractProcessingHandler
     protected function write(array $record) : void
     {
         $record = array_merge($record, [
-            'timestamp'  => $record['datetime']->getTimestamp(),
+            'timestamp' => $record['datetime']->getTimestamp(),
             'stacktrace' => SentryLogger::backtrace($record),
         ]);
 
         if (
                 isset($record['context']['exception']) &&
                 $record['context']['exception'] instanceof \Throwable
-            ) {
+        ) {
             $this->client->getSDK()->captureException($record['context']['exception']);
         } else {
-            $this->client->getSDK()->captureMessage($record['formatted'], new Severity(strtolower($record['level_name'])));
+            $this->client->getSDK()->captureMessage($record['formatted'], new Severity(self::process_severity($record['level_name'])));
         }
     }
 
@@ -81,4 +82,65 @@ class SentryHandler extends AbstractProcessingHandler
     {
         return $this->client;
     }
+
+    /**
+     * Maps PHP's internal error-types into those suited to {@link Severity}.
+     *
+     * @param  mixed int|string $severity The incoming level from userland code or
+     *                                    PHP itself.
+     * @return string
+     */
+    private static function process_severity($severity) : string
+    {
+        // Stringified PHP severities out of \backtrace() like "notice"
+        if (is_string($severity)) {
+            $level = self::from_error($severity);
+        // De-facto PHP severities as constants (ints) like E_NOTICE
+        } else if (is_numeric($severity)) {
+            $level = Severity::fromError($severity);
+        } else {
+            // "Other"
+            $level = Severity::ERROR;
+        }
+
+        return strtolower($level);
+    }
+
+    /**
+     * Almost an exact replica of {@link Severity::fromError()}, except we're
+     * dealing with string values passed to us from upstream processes.
+     * 
+     * @param  string $severity An incoming severity.
+     * @return string
+     */
+    private static function from_error(string $severity) : string
+    {
+        $severity = strtolower($severity);
+        
+        switch ($severity) {
+            case 'deprecated':
+            case 'user_deprecated':
+            case 'warning':
+            case 'user_warning':
+                return Severity::WARNING;
+            case 'error':
+            case 'parse':
+            case 'coreerror':
+            case 'corwarning';
+            case 'compilerrror':
+            case 'compilewarning':
+                return Severity::FATAL;
+            case 'recoverablerror':
+            case 'user_error':
+                return Severity::ERROR;
+            case 'notice':
+            case 'user_notice':
+            case 'strict':
+                return Severity::INFO;
+            default:
+                // It's an error until proven otherwise
+                return Severity::ERROR;
+        }
+    }
+
 }
