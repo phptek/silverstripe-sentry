c6cb093d8b282bf48e12f00b8dc81c493341f8bb
Matias Halles <matias.halles@ackama.com>
2019-11-14 17:14:47 +1300

    Updates manual test task to use Sentry's SDK

--- src/Tasks/TestConnectionTask.php
+++ src/Tasks/TestConnectionTask.php
@@ -2,28 +2,42 @@
 
 namespace PhpTek\Sentry\Tasks;
 
-use Sentry\Severity;
 use SilverStripe\Dev\BuildTask;
-use function Sentry\captureMessage;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Injector\Injector;
+use Monolog\Logger;
+use PhpTek\Sentry\Handler\SentryHandler;
 
 class TestConnectionTask extends BuildTask
 {
     protected $title = 'Test Sentry Configuration';
 
-    protected $description = 'Captures message for all levels available';
+    protected $description = 'Triggers tesk task and sends an Error to Sentry';
 
     /**
-     * Implement this method in the task subclass to
-     * execute via the TaskRunner
+     * Tests that connection is successful via a Task
      *
      * @param \SilverStripe\Control\HTTPRequest|null $request
      */
     public function run($request = null)
     {
-        /**
-         * Tests that connection is successful
-         */
-        $response = captureMessage('Testing Sentry Connection with level WARNING', new Severity(Severity::WARNING));
-        printf("Testing Sentry Connection with level WARNING: %s <br/>\n", ($response ? 'OK': 'FAILED'));
+        /** @var Logger $logger */
+        $logger = Injector::inst()->createWithArgs(Logger::class, ['error-log'])
+            ->pushHandler(SentryHandler::create());
+
+        // Send an `ERROR` level message
+        $logger->error('Testing Sentry Connection with level ERROR');
+        self::output("Testing Sentry Connection with level ERROR");
     }
+
+    /**
+     * Prints out message
+     * @param string $message
+     */
+    private static function output(string $message)
+    {
+        $newLine = Director::is_cli() ? PHP_EOL : '<br/>';
+        printf($message . $newLine);
+    }
+
 }
